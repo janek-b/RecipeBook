5cde0fff520c9cdbd9ecae6e57e8ae7393c7d8e5
Janek Brandt <brandt.janek@gmail.com>
2017-05-26 21:00:43 -0700

    add font to fragment titles

--- app/src/main/java/com/janek/recipebook/About.java
+++ app/src/main/java/com/janek/recipebook/About.java
@@ -1,17 +1,26 @@
 package com.janek.recipebook;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class About extends Fragment {
+  @Bind(R.id.about_title) TextView mTitle;
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.about, container, false);
+    ButterKnife.bind(this, view);
+    Typeface raleway = Typeface.createFromAsset(getActivity().getAssets(), "fonts/raleway-regular.ttf");
+    mTitle.setTypeface(raleway);
     return view;
   }
 
--- app/src/main/java/com/janek/recipebook/Home.java
+++ app/src/main/java/com/janek/recipebook/Home.java
@@ -1,18 +1,27 @@
 package com.janek.recipebook;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class Home extends Fragment {
+  @Bind(R.id.home_title) TextView mTitle;
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.home, container, false);
+    ButterKnife.bind(this, view);
+    Typeface raleway = Typeface.createFromAsset(getActivity().getAssets(), "fonts/raleway-regular.ttf");
+    mTitle.setTypeface(raleway);
     return view;
   }
 
