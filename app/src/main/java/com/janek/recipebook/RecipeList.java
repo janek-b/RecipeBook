6e865c743d27b8fad2fa30123ab09368ee98ddc1
Janek Brandt <brandt.janek@gmail.com>
2017-05-26 21:56:08 -0700

    add recipe list fragments

--- app/src/main/java/com/janek/recipebook/RecipeList.java
+++ app/src/main/java/com/janek/recipebook/RecipeList.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -14,6 +16,7 @@ import butterknife.ButterKnife;
 
 public class RecipeList extends Fragment {
   @Bind(R.id.recipe_list_title) TextView mTitle;
+  @Bind(R.id.recipe_list) ListView mRecipeList;
 
   @Nullable
   @Override
@@ -22,6 +25,8 @@ public class RecipeList extends Fragment {
     ButterKnife.bind(this, view);
     Typeface raleway = Typeface.createFromAsset(getActivity().getAssets(), "fonts/raleway-regular.ttf");
     mTitle.setTypeface(raleway);
+    ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.recipes));
+    mRecipeList.setAdapter(adapter);
     return view;
   }
 
