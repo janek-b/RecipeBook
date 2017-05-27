b2a008f1f98f64ad3b57f7bb89fe96ea52d6f7e6
Janek Brandt <brandt.janek@gmail.com>
2017-05-27 13:49:51 -0700

    add click listeners to recipe list items

--- app/src/main/java/com/janek/recipebook/RecipeList.java
+++ app/src/main/java/com/janek/recipebook/RecipeList.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,11 +22,20 @@ public class RecipeList extends Fragment {
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.recipe_list, container, false);
     ButterKnife.bind(this, view);
-    
+
     String[] recipeNames = getResources().getStringArray(R.array.recipe_name);
     String[] recipeDesc = getResources().getStringArray(R.array.recipe_description);
-
+//    ((MainActivity)getActivity()).loadFragment();
     mRecipeList.setAdapter(new RecipeListAdapter(getActivity(), recipeNames, recipeDesc));
+
+    mRecipeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+        String recipe = ((TextView)view.findViewById(R.id.recipe_title)).getText().toString();
+        Toast.makeText(getActivity(), recipe, Toast.LENGTH_LONG).show();
+      }
+    });
     return view;
   }
 
