b309d5d602ad5b011d418b58c04f8c4a0d2f851a
Janek Brandt <brandt.janek@gmail.com>
2017-05-29 17:48:31 -0700

    add more content to about page

--- app/src/main/java/com/janek/recipebook/ui/AboutFragment.java
+++ app/src/main/java/com/janek/recipebook/ui/AboutFragment.java
@@ -4,6 +4,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import butterknife.ButterKnife;
 
 public class AboutFragment extends Fragment {
   @Bind(R.id.about_title) TextView mTitle;
+  @Bind(R.id.about_link) TextView mLink;
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,13 +25,14 @@ public class AboutFragment extends Fragment {
     ButterKnife.bind(this, view);
     Typeface raleway = Typeface.createFromAsset(getActivity().getAssets(), "fonts/raleway-regular.ttf");
     mTitle.setTypeface(raleway);
+    mLink.setMovementMethod(LinkMovementMethod.getInstance());
     return view;
   }
 
   @Override
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    getActivity().setTitle("AboutFragment");
+    getActivity().setTitle("About");
 
   }
 }
