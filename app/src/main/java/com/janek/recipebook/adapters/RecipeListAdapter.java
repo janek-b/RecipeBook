15cd979170cfa03a4cb8fca5b305ea83a0f02282
Janek Brandt <brandt.janek@gmail.com>
2017-06-02 19:01:19 -0700

    change recipe list to use recycler view instead of list view

--- app/src/main/java/com/janek/recipebook/adapters/RecipeListAdapter.java
+++ app/src/main/java/com/janek/recipebook/adapters/RecipeListAdapter.java
@@ -1,63 +1,105 @@
 package com.janek.recipebook.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.janek.recipebook.R;
+import com.janek.recipebook.models.Recipe;
 import com.janek.recipebook.models.RecipeList;
 import com.janek.recipebook.models.RecipeListResponse;
+import com.janek.recipebook.services.SpoonClient;
+import com.janek.recipebook.services.SpoonService;
+import com.janek.recipebook.ui.MainActivity;
+import com.janek.recipebook.ui.RecipeDetailFragment;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class RecipeListAdapter extends BaseAdapter {
-  private static final int MAX_WIDTH = 400;
-  private static final int MAX_HEIGHT = 300;
 
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder> {
   private Context mContext;
   private List<RecipeList> mRecipes;
   private String mImageBaseUrl;
 
-  public RecipeListAdapter(Context context, RecipeListResponse recipeListResponse) {
+  public RecipeListAdapter(Context context, RecipeListResponse recipeResponse) {
     this.mContext = context;
-    this.mRecipes = recipeListResponse.getResults();
-    this.mImageBaseUrl = recipeListResponse.getBaseUri();
+    this.mRecipes = recipeResponse.getResults();
+    this.mImageBaseUrl = recipeResponse.getBaseUri();
   }
 
   @Override
-  public int getCount() {
+  public RecipeListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+    RecipeListViewHolder viewHolder = new RecipeListViewHolder(view);
+    return viewHolder;
+  }
+
+  @Override
+  public void onBindViewHolder(RecipeListViewHolder holder, int position) {
+    holder.bindRecipeList(mRecipes.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
     return mRecipes.size();
   }
 
-  @Override
-  public Object getItem(int position) {
-    return null;
-  }
+  public class RecipeListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private static final int MAX_WIDTH = 400;
+    private static final int MAX_HEIGHT = 300;
+    @Bind(R.id.recipe_title) TextView title;
+    @Bind(R.id.recipe_desc) TextView desc;
+    @Bind(R.id.recipe_img) ImageView img;
 
-  @Override
-  public long getItemId(int position) {
-    return 0;
-  }
+    private Context mContext;
 
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    if (convertView == null) {
-      convertView = inflater.inflate(R.layout.recipe_list_item, null);
+    public RecipeListViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
+      mContext = itemView.getContext();
+      itemView.setOnClickListener(this);
     }
-    TextView title = (TextView) convertView.findViewById(R.id.recipe_title);
-    TextView desc = (TextView) convertView.findViewById(R.id.recipe_desc);
-    ImageView img = (ImageView) convertView.findViewById(R.id.recipe_img);
-    title.setText(mRecipes.get(position).getTitle());
-    desc.setText(String.format("Cook Time: %d minutes", mRecipes.get(position).getCookTime()));
-    Picasso.with(convertView.getContext()).load(String.format("%s%s", mImageBaseUrl, mRecipes.get(position).getImage()))
-        .resize(MAX_WIDTH, MAX_HEIGHT).centerCrop().into(img);
-    return convertView;
+
+    public void bindRecipeList(RecipeList recipeList) {
+      title.setText(recipeList.getTitle());
+      desc.setText(String.format("Cook Time: %d minutes", recipeList.getCookTime()));
+      Picasso.with(mContext).load(String.format("%s%s", mImageBaseUrl, recipeList.getImage()))
+          .resize(MAX_WIDTH, MAX_HEIGHT).centerCrop().into(img);
+    }
+    @Override
+    public void onClick(View v) {
+      final ProgressDialog loading = new ProgressDialog(mContext);
+      loading.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+      loading.setMessage("Getting recipe Details...");
+      loading.setIndeterminate(true);
+      loading.show();
+
+      RecipeList recipe = mRecipes.get(getLayoutPosition());
+
+      SpoonClient spoonClient = SpoonService.createService(SpoonClient.class);
+      Call<Recipe> call = spoonClient.getRecipe(recipe.getId());
+      call.enqueue(new Callback<Recipe>() {
+        @Override
+        public void onResponse(Call<Recipe> call, Response<Recipe> response) {
+          RecipeDetailFragment recipeDetailFragment = RecipeDetailFragment.newInstance(response.body());
+
+          ((MainActivity)mContext).loadFragment(recipeDetailFragment);
+          loading.dismiss();
+        }
+        @Override public void onFailure(Call<Recipe> call, Throwable t) {t.printStackTrace();}
+      });
+    }
   }
 }
--- app/src/main/java/com/janek/recipebook/ui/RecipeListFragment.java
+++ app/src/main/java/com/janek/recipebook/ui/RecipeListFragment.java
@@ -1,40 +1,25 @@
 package com.janek.recipebook.ui;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.janek.recipebook.R;
 import com.janek.recipebook.adapters.RecipeListAdapter;
-import com.janek.recipebook.models.Recipe;
-import com.janek.recipebook.models.RecipeList;
 import com.janek.recipebook.models.RecipeListResponse;
-import com.janek.recipebook.services.SpoonClient;
-import com.janek.recipebook.services.SpoonService;
 
 import org.parceler.Parcels;
 
-import java.io.IOException;
-import java.util.List;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class RecipeListFragment extends Fragment {
-  @Bind(R.id.recipe_list) ListView mRecipeList;
+  @Bind(R.id.recipe_list) RecyclerView mRecipeList;
 
   private RecipeListResponse recipeResponse;
 
@@ -58,29 +43,9 @@ public class RecipeListFragment extends Fragment {
     View view = inflater.inflate(R.layout.recipe_list, container, false);
     ButterKnife.bind(this, view);
     mRecipeList.setAdapter(new RecipeListAdapter(getActivity(), recipeResponse));
-
-    mRecipeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      @Override
-      public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        final ProgressDialog loading = new ProgressDialog(getContext());
-        loading.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        loading.setMessage("Getting recipe Details...");
-        loading.setIndeterminate(true);
-        loading.show();
-
-        SpoonClient spoonClient = SpoonService.createService(SpoonClient.class);
-        Call<Recipe> call = spoonClient.getRecipe(recipeResponse.getResults().get(position).getId());
-        call.enqueue(new Callback<Recipe>() {
-          @Override
-          public void onResponse(Call<Recipe> call, Response<Recipe> response) {
-            RecipeDetailFragment recipeDetailFragment = RecipeDetailFragment.newInstance(response.body());
-            ((MainActivity)getActivity()).loadFragment(recipeDetailFragment);
-            loading.dismiss();
-          }
-          @Override public void onFailure(Call<Recipe> call, Throwable t) {t.printStackTrace();}
-        });
-      }
-    });
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+    mRecipeList.setLayoutManager(layoutManager);
+    mRecipeList.setHasFixedSize(true);
     return view;
   }
 
