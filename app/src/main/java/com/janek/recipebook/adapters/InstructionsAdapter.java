624cf2adad395c716fdfd84765261832df012832
Janek Brandt <brandt.janek@gmail.com>
2017-06-05 16:17:37 -0700

    put instruction steps into card views and use constraint layout

--- app/src/main/java/com/janek/recipebook/adapters/InstructionsAdapter.java
+++ app/src/main/java/com/janek/recipebook/adapters/InstructionsAdapter.java
@@ -1,14 +1,17 @@
 package com.janek.recipebook.adapters;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.janek.recipebook.R;
 import com.janek.recipebook.models.Instruction;
+import com.janek.recipebook.models.Item;
 import com.janek.recipebook.models.Step;
 
 import java.util.List;
@@ -43,6 +46,8 @@ public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsAdapte
   public class InstructionsViewHolder extends RecyclerView.ViewHolder {
     @Bind(R.id.step_number) TextView stepNumberTextView;
     @Bind(R.id.step_desc) TextView stepDescTextView;
+    @Bind(R.id.ingredients_instructions_label) TextView ingredientLabel;
+    @Bind(R.id.step_layout) LinearLayout stepLayout;
 
     private Context mContext;
 
@@ -55,6 +60,17 @@ public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsAdapte
     public void bindInstructions(Step step) {
       stepNumberTextView.setText(String.format("%d", step.getNumber()));
       stepDescTextView.setText(step.getStep());
+      if (step.getIngredients().size() > 0) {
+        ingredientLabel.setVisibility(View.VISIBLE);
+      } else {
+        ingredientLabel.setVisibility(View.GONE);
+      }
+      for (Item ingredient : step.getIngredients()) {
+        TextView ingredientView = new TextView(mContext);
+        ingredientView.setText(ingredient.getName());
+        ingredientView.setTextColor(ContextCompat.getColor(mContext, R.color.colorTextIcons));
+        stepLayout.addView(ingredientView);
+      }
     }
   }
 }
